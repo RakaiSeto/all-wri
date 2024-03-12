f1f769027f78f35eefdf46ad0cbf37642cc9b6a8
Rakai Seto <mamangrakai@gmail.com>
2024-03-13 01:31:29 +0700

    feat : improve time day 1

--- technicallyRamadhan2024/day-1/main.go
+++ technicallyRamadhan2024/day-1/main.go
@@ -18,15 +18,9 @@ func SpinWords(str string) string {
 	return strings.Join(words, " ")
 } // SpinWords
 
-func reverse(s string) string {
-	// given a string, reverse the string
-	// convert the string to a slice of runes
-	runes := []rune(s)
-	// iterate through the slice of runes
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		// swap the runes
-		runes[i], runes[j] = runes[j], runes[i]
+func reverse(str string) (result string) {
+	for _, v := range str {
+		result = string(v) + result
 	}
-	// convert the slice of runes back to a string
-	return string(runes)
+	return
 }
